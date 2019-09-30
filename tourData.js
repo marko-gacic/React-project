abf8295493d63cf7201291fbe35a30402f486ce7
unknown <poslovnogacic@gmail.com>
2019-09-30 23:39:21 +0200

    first commit

---
+++ tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Take a relaxing stroll across the Brooklyn Bridge and see some of the great sights of NYC on the Brooklyn Bridge Sightseeing Walking Tour.Come see the beauty and history of lower Manhattan and the Brooklyn Bridge at an easy pace with one of New York’s best tour guides. This walking tour will turn you into an expert on New York’s history and a lover of its amazing views."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Paris by night is just MAGIC!Discover Paris, city of lights, and its most beautiful monuments. Paris is shining at nightfall. Every night, visit Paris by night by double-decker bus with open roof (or closed roof in winter) with individual audio guides (9 languages : French, English, Spanish, Italian, Russian, Japanese, Portuguese, German)."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Take a sightseeing tour of London and learn from experts as they show you the city's best attractions and famous landmarks. Learn interesting historical and cultural facts about London on a walking tour, a double decker bus tour, a river Thames tour, or a even a helicopter tour.Book a one-day hop on hop off bus tour and get an extra 24 hours free"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Discover Tokyo's must-see landmarks on this full-day coach tour including a cruise along the Tokyo Bay. Drive past the National Theater and other major attractions en route to the Imperial Palace East Garden for a stroll. Then explore the sights of Asakusa, including Senso-ji Temple, with commentary from an audio guide. Hop aboard a short cruise on Tokyo Bay. Opt to include a buffet lunch if you’d like, with views of the Bay and the Rainbow Bridge from your seat."
+  }
+];
